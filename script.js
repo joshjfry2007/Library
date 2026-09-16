dc0b26d78df65f8524dbd1fd486198dca4e15d83
Joshua Jeffrey L <joshjfry2007@gmail.com>
2026-09-16 19:48:01 +0530

    Enhanced functionality with delete and Toggler for read buttons

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function Book(title, author, pages, haveRead){
     this.info = function(){
         console.log(`${this.title} by ${this.author} of ${pages} ; ${haveRead}`);
     }
+    Book.prototype.toggleRead = function () {
+        this.haveRead = !this.haveRead;
+        display();
+    };
 }
 
 function addBookToLibrary(title, author, pages, haveRead){
@@ -22,17 +26,36 @@ function display(){
     myLib.forEach((book)  =>{
         const card = document.createElement("div");
         card.classList.add("book-card")
-        // card.style.backgroundColor = "rgb(195, 157, 231)"
+        let r = Math.floor(Math.random() * 86);
+        let g = Math.floor(Math.random() * 86);
+        let b = Math.floor(Math.random() * 86);
+        card.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
         // card.style.borderRadius = "10px"
         // card.style.padding = "10px 10px 10px 10px"
 
         card.setAttribute("data-id", book.id);
         card.innerHTML = 
-            `<p>${book.title}</p>
-            <p>${book.author}</p>
-            <p>${book.pages}</p>
-            <p>${book.haveRead ? "Read" : "Not Read"}</p>`
-        
+            `<h4>Name : ${book.title}</h4>
+            <p>Author : ${book.author}</p>
+            <p>Number of pages : ${book.pages}</p>
+            <p>${book.haveRead ? "Read" : "Not Read"}</p>
+            <button class="toggle-read-btn">Toggle Read</button>`
+        const delBtn = document.createElement("button");
+        delBtn.textContent = "Delete";
+        delBtn.style.padding = "5px 10px"
+        // delBtn.style.marginBottom = "10px"
+        delBtn.addEventListener("click", () => {
+            const index = myLib.findIndex((book) => book.id === card.dataset.id);
+            myLib.splice(index,1);
+            cards.removeChild(card);
+            display();
+        })
+        const toggleBtn = card.querySelector(".toggle-read-btn");
+        toggleBtn.addEventListener("click", () => {
+            book.toggleRead();
+            display();
+        });
+        card.appendChild(delBtn);
         cards.appendChild(card);
     })
 }
